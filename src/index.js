7ceadb6218ebe01cb6b54da9e4bbdbc8bbcb105f
Uday Sairam Kommineni <67004722+uday79936@users.noreply.github.com>
2025-12-19 12:05:57 +0530

    Wrap App component in React.StrictMode

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+// Future providers (Theme / Redux / Router) can easily plug in here
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
